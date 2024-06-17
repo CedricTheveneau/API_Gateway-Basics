33c7850970d5a48b3d70c303740510d7d9d355c9
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-06-17 16:42:48 +0200

    Added bridge between API Gateway and Auth Micro-service

--- Auth/app/routes/index.js
+++ Auth/app/routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express();
 const authRoutes = require("./auth.js");
 
-router.use("/auth", authRoutes);
+router.use("/", authRoutes);
 
 module.exports = router;
